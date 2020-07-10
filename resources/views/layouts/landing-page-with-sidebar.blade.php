3a3162302e94b2c3547a95508db864f39d21b6c6
Jassim Ahmed <bdjoejassim@gmail.com>
2020-07-10 16:11:40 -0400

    Completed sidebar

--- resources/views/layouts/landing-page-with-sidebar.blade.php
+++ resources/views/layouts/landing-page-with-sidebar.blade.php
@@ -10,9 +10,15 @@
 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+    {{-- Fontawesome --}}
+    {{-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> --}}
+    <script src="https://kit.fontawesome.com/3cd501a181.js" crossorigin="anonymous"></script>
+
+
     <!-- Our Custom CSS -->
     <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/sidebar.css">
 
     <!-- Font Awesome JS -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
@@ -26,72 +32,93 @@
     <div class="wrapper">
         <!-- Sidebar  -->
         <nav id="sidebar">
-            <div class="sidebar-header">
+            {{-- <div class="sidebar-header">
                 <h3>Bootstrap Sidebar</h3>
+            </div> --}}
+            <div class="top">
+                <div class="logo d-flex justify-content-center mt-5">
+                    <a href="/">
+                        <img height="70" src="{{$settings['general']->logo_img_url}}">
+                    </a>
+                </div>
+                <ul class="list-unstyled components">
+                    <p>{{$settings['general']->site_title}}</p>
+                    {{-- <li class="active"> --}}
+                    <li>
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Menu</a>
+                        <ul class="collapse list-unstyled" id="homeSubmenu">
+                            @foreach ($categories as $category)
+                            <li>
+                                <a href="#">{{$category->title}}</a>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="/reservations">Waitlist</a>
+                    </li>
+                    <li>
+                        <a href="/offers">Offers</a>
+                    </li>
+                    <li>
+                        <a href="/about">More</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
             </div>
-
-            <ul class="list-unstyled components">
-                <p>Dummy Heading</p>
-                <li class="active">
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
-                    <ul class="collapse list-unstyled" id="homeSubmenu">
-                        <li>
-                            <a href="#">Home 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Home 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Home 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-
-            <ul class="list-unstyled CTAs">
-                <li>
-                    <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
-                </li>
-                <li>
-                    <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
-                </li>
-            </ul>
+            <div class="bottom container-fluid p-2">
+                    <div class="location col-12 p-0">
+            			<div class="address">
+            				{{$settings['general']->address_1}} {{$settings['general']->address_2}} <br>
+            				{{$settings['general']->postalcode}} {{$settings['general']->city}}, {{$settings['general']->province}}, Canada.
+            			</div>
+            			<div class="phone-number mt-2">
+            				<a href="tel:{{$settings['general']->phone_number}}" >{{$settings['general']->phone_number}}</a>
+            			</div>
+            		</div>
+                    <div class="delivery-options col-12 p-0 mt-2">
+            			Livraison • Pick-Up
+            		</div>
+            		<div class="social-icons col-12 p-0 mt-2">
+            			<a href="{{$settings['social']->facebook_url}}" class="cdn-icons">
+            				<i class="fab fa-facebook"></i>
+            			</a>
+            			<a href="{{$settings['social']->twitter_url}}" class="cdn-icons">
+            				<i class="fab fa-twitter" aria-hidden="true"></i>
+            			</a>
+            			<a href="{{$settings['social']->instagram_url}}" class="cdn-icons">
+            				<i class="fab fa-instagram" aria-hidden="true"></i>
+            			</a>
+            			<a href="#" class="cdn-icons">
+            				<i class="fab fa-youtube" aria-hidden="true"></i>
+            			</a>
+            		</div>
+            </div>
         </nav>
 
 
         <section id="app-layout">
             <div class="container-fluid">
                 {{-- Topbar --}}
-                <div class="row">
-                    <div class="col-12 topbar d-md-none p-3 d-flex align-items-center justify-content-left">
-                        <button type="button" id="sidebarCollapse" class="btn btn-info">
-                            <i class="fas fa-align-left"></i>
+                <div class="row topbar d-md-none p-2">
+                    <div class="col-4 p-0 d-flex justify-content-left">
+                        <button type="button" id="sidebarCollapse" class="btn btn-dark">
+                            <i class="fas fa-bars"></i>
+                            {{-- <i class="fas fa-align-left"></i> --}}
                             <span>Toggle Sidebar</span>
                         </button>
-                        Something
+                    </div>
+                    <div class="col-4 d-flex justify-content-center">
+                        <div class="logo ">
+                    		<a href="/">
+                    			<img height="40" src="{{$settings['general']->logo_img_url}}">
+                    		</a>
+                    	</div>
+                    </div>
+                    <div class="col-4">
+
                     </div>
                 </div>
                 {{-- Welcome box --}}
@@ -105,26 +132,6 @@
                   </div>
                 </div>
             </div>
-            {{-- Topbar --}}
-            {{-- <div class="container-fluid topbar d-md-none p-3 d-flex align-items-center justify-content-left">
-                <div class="row">
-                    <div class="">
-                        <button type="button" id="sidebarCollapse" class="btn btn-info">
-                            <i class="fas fa-align-left"></i>
-                            <span>Toggle Sidebar</span>
-                        </button>
-                        Something
-                    </div>
-                </div>
-            </div> --}}
-            {{-- Welcome box --}}
-          {{-- <div class="welcome-jumbo container-fluid">
-            <div class="welcome-message">
-              <div class="status">  The New </div>
-              <h1>Billy Burger</h1>
-            </div>
-            <img src="https://i.ya-webdesign.com/images/hamburger-and-fries-png-4.png" class="burger-fries">
-          </div> --}}
         </section>
     </div>
 
